cf81530388b6ff5a12e1c88bcd020c08071fcd0f
renhaizhen <443485837@qq.com>
2021-10-12 17:40:18 +0800

    -

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 // import ES from './echart/_index';
 import ECHART from './echart/index';
+// import './script/main'
 function App() {
   return (
     <div className="App">
       {/* <ES/> */}
-      <ECHART/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadssss .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React l
-        </a> 
-      </header> */}
+      {/* <ECHART/> */}
+      <div>hello heson</div>
     </div>
   );
 }
---
+++ src/script/sw.js
@@ -0,0 +1,4 @@
+console.log(12344)
+this.addEventListener('install', function (event) {
+    console.log('Service Worker install');
+  });
