b142fb4f70f29c0d94b576234973976350145c7f
renhaizhen <443485837@qq.com>
2021-10-19 14:33:23 +0800

    --

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+
 // import ES from './echart/_index';
 import ECHART from './echart/index';
-// import './script/main'
 function App() {
   return (
     <div className="App">
--- src/script/sw.js
+++
@@ -1,4 +0,0 @@
-console.log(12344)
-this.addEventListener('install', function (event) {
-    console.log('Service Worker install');
-  });
